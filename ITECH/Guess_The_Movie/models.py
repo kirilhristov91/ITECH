211cd7cf3bce6765db5c227d61a7e60b0bea355f
Kiril Hristov <kirilhristov91@gmail.com>
2016-02-27 13:17:02 +0000

    change models to extend the predefined django user

--- ITECH/Guess_The_Movie/models.py
+++ ITECH/Guess_The_Movie/models.py
@@ -1,34 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class UserProfile (models.Model):
 
-class User (models.Model):
-    username = models.CharField(max_length=128, unique=True)
-    password = models.CharField(max_length=64)
-    email = models.CharField(max_length=128, unique=True)
+    user = models.OneToOneField(User)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
     game_session_counter = models.IntegerField(default=0)
     total_points = models.IntegerField(default=0)
 
     def __unicode__(self):
-        return self.username
-
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User)
-    avatar = models.ImageField()
+        return self.user.username
 
 
 class GameSession (models.Model):
-    user_id = models.ForeignKey(User)
+    user_id = models.ForeignKey(UserProfile)
     timestamp = models.DateTimeField(auto_now_add=True)
     points = models.IntegerField(defaul=0)
 
 
 class Question (models.Model):
     game_session_id = models.ForeignKey(GameSession)
-    option_a = models.CharField(max_lenght=128)
-    option_b = models.CharField(max_lenght=128)
-    option_c = models.CharField(max_lenght=128)
-    option_d = models.CharField(max_lenght=128)
+    option_a = models.CharField(max_lenght=256)
+    option_b = models.CharField(max_lenght=256)
+    option_c = models.CharField(max_lenght=256)
+    option_d = models.CharField(max_lenght=256)
     image = models.URLField()
     correct_answer = models.IntegerField(defaul=0)
     selected = models.IntegerField(defaul=0)
