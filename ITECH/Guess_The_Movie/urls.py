a82268b3e18f632f4a717d5c9019372cb839a623
Kiril Hristov <kirilhristov91@gmail.com>
2016-02-27 13:40:03 +0000

    url to index

--- ITECH/Guess_The_Movie/urls.py
+++ ITECH/Guess_The_Movie/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url
 from Guess_The_Movie import views
 
 urlpatterns = patterns('',
+        url(r'^$', views.index, name='index'),
         url(r'^user/(?P<user_name>[\w\-]+)/$', views.userView, name='user'),
         url(r'^question/(?P<questionID>[\w\-]+)/$', views.guestionView, name='guestionview'),)
 
