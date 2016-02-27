399ae730d2cf457e0efe84499581bc06a69af5fd
Kiril Hristov <kirilhristov91@gmail.com>
2016-02-27 14:27:12 +0000

    admin interface done

--- ITECH/Guess_The_Movie/admin.py
+++ ITECH/Guess_The_Movie/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from Guess_The_Movie.models import UserProfile, Question, GameSession
 
-# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+   list_display = ('user', 'picture', 'game_session_counter', 'total_points')
+
+
+class GameSessionAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'timestamp', 'points')
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('game_session_id', 'option_a', 'option_b', 'option_c', 'option_d', 'correct_answer', 'selected')
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(GameSession, GameSessionAdmin)
+admin.site.register(Question, QuestionAdmin)
--- ITECH/ITECH/urls.py
+++ ITECH/ITECH/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'ITECH.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     #
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', include('Guess_The_Movie.urls')), # ADD THIS NEW TUPLE!
 )
