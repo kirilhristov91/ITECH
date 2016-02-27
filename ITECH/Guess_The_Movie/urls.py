c5d5666db04fe249c39cb34bf311e328a5568879
krisodimitrov <kriso.dimitrov@gmail.com>
2016-02-27 15:19:36 +0000

    updating url patterns

--- ITECH/Guess_The_Movie/urls.py
+++ ITECH/Guess_The_Movie/urls.py
@@ -3,8 +3,8 @@ from Guess_The_Movie import views
 
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
-        url(r'^user/(?P<user_name>[\w\-]+)/$', views.userView, name='user'),
-        url(r'^question/(?P<questionID>[\w\-]+)/$', views.guestionView, name='guestionview'),
-        url(r'^register/$', views.register, name='register'), # ADD NEW PATTERN!
+        url(r'^/user/(?P<user_name>[\w\-]+)/$', views.userView, name='user'),
+        url(r'^/question/(?P<questionID>[\w\-]+)/$', views.guestionView, name='guestionview'),
+        url(r'^/register/$', views.register, name='register'), # ADD NEW PATTERN!
     )
 
