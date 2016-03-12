79eda18e61d194476574c5c0db16b2ff25fca8ee
vbakayov <viktorbakayov@gmail.com>
2016-03-12 16:40:46 +0200

    AJAX commit

---
+++ ITECH/static/js/rango-ajax.js
@@ -0,0 +1,11 @@
+ $(document).ready(function(e) {
+    $('#likes').click(function(){
+    console.log("pressed");
+    var catid;
+    queid = $(this).attr("data-queid");
+    $.get('/guess_the_movie/question/', {question_id: queid}, function(data){
+               $('#like_count').html(data);
+
+    });
+    });
+});
