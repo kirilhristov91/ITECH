949666557a1df518e1894dc3041cc33fc8147585
vbakayov <viktorbakayov@gmail.com>
2016-03-12 16:34:45 +0200

    Revert "add AJAX functionality"
    This reverts commit cc2216678b15f8666b9668755b6291f8e24cdbf0.

--- ITECH/Guess_The_Movie/urls.py
+++ ITECH/Guess_The_Movie/urls.py
@@ -4,11 +4,10 @@ from Guess_The_Movie import views
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
         url(r'^user/(?P<user_name>[\w\-]+)/$', views.userView, name='user'),
-         url(r'^question/$', views.question, name='like_category'),
-        #url(r'^question/(?P<questionID>[\w\-]+)/$', views.guestionView, name='guestionview'),
+        url(r'^question/(?P<questionID>[\w\-]+)/$', views.guestionView, name='guestionview'),
         url(r'^register/$', views.register, name='register'),
         url(r'^login/$', views.user_login, name='login'),
         url(r'^logout/$', views.user_logout, name='logout'),
-        url(r'^game/$', views.game_session, name='login'),
+         url(r'^game/$', views.game_session, name='login'),
     )
 
