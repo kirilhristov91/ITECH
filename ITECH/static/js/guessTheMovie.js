af78314611230ff79090a7360689c33f5c6c5487
krisodimitrov <kriso.dimitrov@gmail.com>
2016-03-21 11:24:39 +0000

    separating js from html

---
+++ ITECH/static/js/guessTheMovie.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+     $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    });
+
+      $('#goToBottom').click(function(){
+          var WH = $(window).height();
+          var SH = $('body')[0].scrollHeight;
+          $('html, body').stop().animate({scrollTop: SH-WH}, 1000);
+
+          console.log( SH+' '+WH ); // TEST
+
+      });
+
+    $(".summaryButton").click(function (event) {
+        var element = this;
+        $(this).attr("disabled", true);
+        $.ajax({
+            url: "/guess_the_movie/summary/" + element.id+"/update/",
+            method: "GET",
+            data: {},
+            success: function (result) {
+            }
+        });
+    });
+
+        $( '#table' ).searchable({
+        striped: true,
+        oddRow: { 'background-color': '#f5f5f5' },
+        evenRow: { 'background-color': '#fff' },
+        searchType: 'fuzzy'
+    });
+
+    $( '#searchable-container' ).searchable({
+        searchField: '#container-search',
+        selector: '.row',
+        childSelector: '.col-xs-4',
+        show: function( elem ) {
+            elem.slideDown(100);
+        },
+        hide: function( elem ) {
+            elem.slideUp( 100 );
+        }
+    });
+
+
+     var $table = $('table.scroll'),
+    $bodyCells = $table.find('tbody tr:first').children(),
+    colWidth;
+
+// Adjust the width of thead cells when window resizes
+$(window).resize(function() {
+    // Get the tbody columns width array
+    colWidth = $bodyCells.map(function() {
+        return $(this).width();
+    }).get();
+
+    // Set the width of thead columns
+    $table.find('thead tr').children().each(function(i, v) {
+        $(v).width(colWidth[i]);
+    });
+}).resize(); // Trigger resize handler
+
+});
