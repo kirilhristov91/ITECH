b70c0a97c8da59f0d2ebe85c946e9b5882d4b80d
Kiril Hristov <kirilhristov91@gmail.com>
2016-03-22 18:24:29 +0000

    refactoring urls and views to get rid of unused functions and urls; adding comments on game session and get_movies()

--- ITECH/Guess_The_Movie/views.py
+++ ITECH/Guess_The_Movie/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 
-
 from Guess_The_Movie.models import User
 from Guess_The_Movie.models import UserProfile
 from Guess_The_Movie.models import GameSession
@@ -14,93 +13,75 @@ from Guess_The_Movie.models import Favourites
 from Guess_The_Movie.forms import UserForm, UserProfileForm, resetPasswordForm
 
 from random import randint
-
 import unicodedata
+
 # Create your views here.
 
 coding = 'utf-8'
-
-
-def userView(request, user_name):
-     # Create a context dictionary which we can pass to the template rendering engine.
-    context_dict = {}
-    try:
-        userObject = User.objects.get(username=user_name)
-        context_dict['username'] = userObject
-
-    except User.UserDoesNotExist:
-        #User does not exist
-        pass
-
-    # Go render the response and return it to the client.
-    return render(request, "guess_the_movie/user.html", context_dict)
-
 
 
 def index(request):
     # Go render the response and return it to the client.
     context_dict = {}
     Userstop5 = UserProfile.objects.order_by('-total_points')[:5]
-    context_dict['top5Users']=  Userstop5
+    context_dict['top5Users'] = Userstop5
     return render(request, "guess_the_movie/index.html", context_dict)
 
 
-def question(request):
-
-    if request.method == 'GET':
-        question_id = request.GET['question_id']
-
-    points = 0
-    if question_id:
-        #Hardcoded to work with the user table change to work with question table
-        user = UserProfile.objects.get(user=1)
-        if user:
-            points = user.total_points + 1
-            user.total_points =  points
-            user.save()
-    return HttpResponse(user.total_points)
-
-
 def game_session(request):
+    # check if the user is logged in
     if request.user.is_authenticated():
-        currentUser = UserProfile.objects.get(user=request.user)
         context_dict = {}
+
+        # add the current user object to the context_dict
+        currentUser = UserProfile.objects.get(user=request.user)
         context_dict["user"] = currentUser
 
+        # create a new game session for the current user
         gameSession = GameSession(user=currentUser)
         gameSession.save()
+        context_dict['game_session'] = gameSession
+
+        # get ten random movies using the get_movies() helper function
         movies_answers = get_movies()
+
+        # add the movies and the answers to the context_dict
         moviesArray = movies_answers['moviesArray']
         answersArray = movies_answers['answersArray']
         context_dict["answers"] = movies_answers['moviesArray']
         context_dict["movies"] = movies_answers['answersArray']
-        context_dict['game_session'] = gameSession
 
-        i=0
+        # create and add to the database the 10 questions for the game session
+        # this is done so that later if the user guess correctly
+        #   a particular question the is_guess_correct field in the database is updated to True
+        i = 0
         while i < len(moviesArray):
             current_movie = moviesArray[i]
             answers = answersArray[i]
-            question = Question(game_session=gameSession, movie=current_movie, index = i)
+            question = Question(game_session=gameSession, movie=current_movie, index=i)
             question.save()
             for answer in answers:
                 Answer(question=question, text=answer).save()
             i = i + 1
 
         return render(request, 'guess_the_movie/game.html', context_dict)
+
+    # if no user is logged in return the login page
     else:
         return render(request, 'guess_the_movie/login.html', {})
 
 
+# set the is_guess_correct field to True if a question during the game session is answered correctly and update the points
 def update_question(request, question_id):
     if request.user.is_authenticated():
         currentUser = UserProfile.objects.get(user=request.user)
 
-        #update question
+        # update question
         question = Question.objects.get(id=question_id)
         question.is_guess_correct = True
         question.save()
 
-        #update users points
+        # update users points
         currentUser.total_points = currentUser.total_points + 1
         currentUser.save()
         return HttpResponse(status=200)
@@ -108,6 +89,7 @@ def update_question(request, question_id):
         return render(request, 'guess_the_movie/login.html', {})
 
 
+# function to get 10 random movies from the database
 def get_movies():
     minId = Movie.objects.order_by('id')[0].id
     maxId = Movie.objects.order_by('-id')[0].id
@@ -116,8 +98,12 @@ def get_movies():
     index = 0
 
     while index < 10:
+        # generate a random id to get the corresponding movie
         randomId = randint(minId, maxId)
         movie = Movie.objects.get(id=randomId)
+
+        # check if the movie is not already present in the movies array
+        # to avoid showing the user the same movie within the same game session
         exist = False
         for m in moviesArray:
             if m.title == movie.title:
@@ -125,61 +111,51 @@ def get_movies():
         if exist == True:
             continue
 
+        # check if the alternative answers for the current movie are more than 3
+        # because some of the movies contain less than three titles in other_options
         options = movie.other_options.split(", ")
         if len(options) < 3:
             continue
 
+        # get rid of weird characters and leave only the title of the movie
         answers = []
-         
         title = movie.title
-        title = unicodedata.normalize('NFKD', title).encode('ascii','ignore')
+        title = unicodedata.normalize('NFKD', title).encode('ascii', 'ignore')
         title = title.replace('"', "")
         title = title.lstrip()
 
-        flag=0
-        size = len(options)-1
-        while flag<3:
-
+        # choose randomly 3 other titles from the other_options
+        flag = 0
+        size = len(options) - 1
+        while flag < 3:
             randomMovie = randint(0, size)
-            #print len(options)
+            # get rid of weird characters and leave only the title of the other option
             a = options.pop(randomMovie)
-            a = unicodedata.normalize('NFKD', a).encode('ascii','ignore')
+            a = unicodedata.normalize('NFKD', a).encode('ascii', 'ignore')
             a = a.lstrip()
             a = a.replace('"', "")
-            a = a.replace('\'','')
-            a = a.replace("]",'')
-            a = a.replace("[",'')
+            a = a.replace('\'', '')
+            a = a.replace("]", '')
+            a = a.replace("[", '')
             a = a.encode('utf-8')
-            print a
-            if(a is title): continue
+            if (a is title):
+                continue
+
+            # store the option in the answers array
             answers.append(a)
             flag = flag + 1
             size = size - 1
 
-
-       
+        # store the title of the movie in the answers array as well
         answers.append(title)
+
+        # sort the array of answers
         answers = sorted(answers)
         answersArray.append(answers)
         moviesArray.append(movie)
         index = index + 1
 
-    return {'moviesArray':moviesArray, 'answersArray':answersArray}
-
-
- # A function to get an existing question
-def guestionView(request, questionID):
-     # Create a context dictionary which we can pass to the template rendering engine.
-    context_dict = {}
-    try:
-        userObject = Question.objects.get(game_session_id=questionID)
-        context_dict['question'] = userObject
-
-    except Question.UserDoesNotExist:
-        #User does not exist
-        pass
-    # Go render the response and return it to the client.
-    return render(request, "guess_the_movie/question.html", context_dict)
+    return {'moviesArray': moviesArray, 'answersArray': answersArray}
 
 
 def register(request):
@@ -189,105 +165,88 @@ def register(request):
         user_form = UserForm(data=request.POST)
         profile_form = UserProfileForm(data=request.POST)
 
-
         if user_form.is_valid() and profile_form.is_valid():
-
             user = user_form.save()
-
-
             user.set_password(user.password)
             user.save()
-
 
             profile = profile_form.save(commit=False)
             profile.user = user
 
-
             if 'picture' in request.FILES:
                 profile.picture = request.FILES['picture']
 
-
             profile.save()
-
-
             registered = True
-
-
         else:
             print user_form.errors, profile_form.errors
-
-
     else:
         user_form = UserForm()
         profile_form = UserProfileForm()
 
-
     return render(request,
-            'guess_the_movie/register.html',
-            {'user_form': user_form, 'profile_form': profile_form, 'registered': registered} )
+                  'guess_the_movie/register.html',
+                  {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
 
 
 def user_login(request):
-    #If it is POST request, get the information out of it
+    # If it is POST request, get the information out of it
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
 
-        user = authenticate(username=username,password=password)
+        user = authenticate(username=username, password=password)
 
         # If it is a registered user, login
         if user:
-            login(request,user)
+            login(request, user)
             return HttpResponseRedirect('/guess_the_movie/')
-
-
         else:
             print "Invalid login details"
-            return render(request, 'guess_the_movie/login.html', {"fail":True})
-
+            return render(request, 'guess_the_movie/login.html', {"fail": True})
 
     else:
-         return render(request, 'guess_the_movie/login.html', {})
+        return render(request, 'guess_the_movie/login.html', {})
 
 
-def summary(request,game_session_id):
-     print request.user
-     if request.user.is_authenticated():
+def summary(request, game_session_id):
+    print request.user
+    if request.user.is_authenticated():
         currentUser = UserProfile.objects.get(user=request.user)
-        context_dict={}
-        playersAnswers=[]
-        
+        context_dict = {}
+        playersAnswers = []
 
         gameSession = GameSession.objects.get(id=game_session_id)
         if currentUser == gameSession.user:
-              correctAnswersCount = 0
-              answers = Question.objects.filter(game_session=gameSession)
-              playersAnswers=[]
-              for i in range(len(answers)):
-                   alreadyInFav = False
-                   fav = Favourites.objects.filter(movie=answers[i].movie, user = request.user)
-                   if len(fav)!=0:
-                       alreadyInFav = True
-                   playersAnswers.append({'movie': answers[i].movie, 'answered': answers[i].is_guess_correct, 'fav':alreadyInFav})
-                   if answers[i].is_guess_correct:
-                        correctAnswersCount+=1
-        gameSession.points=correctAnswersCount
+            correctAnswersCount = 0
+            answers = Question.objects.filter(game_session=gameSession)
+            playersAnswers = []
+            for i in range(len(answers)):
+                alreadyInFav = False
+                fav = Favourites.objects.filter(movie=answers[i].movie, user=request.user)
+                if len(fav) != 0:
+                    alreadyInFav = True
+                playersAnswers.append(
+                        {'movie': answers[i].movie, 'answered': answers[i].is_guess_correct, 'fav': alreadyInFav})
+                if answers[i].is_guess_correct:
+                    correctAnswersCount += 1
+        gameSession.points = correctAnswersCount
         gameSession.save()
 
-
-        context_dict['answers']= playersAnswers
+        context_dict['answers'] = playersAnswers
         context_dict['correct'] = correctAnswersCount
         print context_dict
         return render(request, 'guess_the_movie/summary.html', context_dict)
-     else:
-         return render(request, 'guess_the_movie/login.html', {})
+
+    else:
+        return render(request, 'guess_the_movie/login.html', {})
 
 
-def add_to_favourites(request,movieId):
+def add_to_favourites(request, movieId):
     if request.user.is_authenticated():
         currentUser = UserProfile.objects.get(user=request.user)
         currentMovie = Movie.objects.get(id=movieId)
-        fav = Favourites(user=currentUser,movie=currentMovie)
+        fav = Favourites(user=currentUser, movie=currentMovie)
         fav.save()
         return HttpResponse(status=200)
 
@@ -295,13 +254,12 @@ def add_to_favourites(request,movieId):
         return render(request, 'guess_the_movie/login.html', {})
 
 
-
-
 def leaderboard(request):
     context_dict = {}
     allUsers = UserProfile.objects.order_by('-total_points')
-    context_dict['allUsers']= allUsers
+    context_dict['allUsers'] = allUsers
     return render(request, "guess_the_movie/leaderboard.html", context_dict)
+
 
 def about(request):
     return render(request, "guess_the_movie/about.html", {})
@@ -314,8 +272,10 @@ def user_logout(request):
     # Take the user back to the homepage.
     return HttpResponseRedirect('/guess_the_movie/')
 
+
 def profile(request):
-    context_dict = {'userp':UserProfile.objects.get(user=request.user), 'userfav':Favourites.objects.filter(user=request.user)}
+    context_dict = {'userp': UserProfile.objects.get(user=request.user),
+                    'userfav': Favourites.objects.filter(user=request.user)}
     numberGames = GameSession.objects.filter(user=request.user)
     max = 0
     sum = 0
@@ -324,54 +284,56 @@ def profile(request):
         tempMax = numberGames[i].points
         if tempMax > max:
             max = tempMax
-        sum +=tempMax
+        sum += tempMax
 
     context_dict['games'] = len(numberGames)
     context_dict['max'] = max
-    if len(numberGames)==0:
-           context_dict['avg'] = 0
+    if len(numberGames) == 0:
+        context_dict['avg'] = 0
     else:
-        context_dict['avg'] = round(float(sum)/len(numberGames),2)
+        context_dict['avg'] = round(float(sum) / len(numberGames), 2)
 
     return render(request, "guess_the_movie/profile.html", context_dict)
 
+
 def upload_picture(request):
-     if request.method == "POST":
-         if request.user.is_authenticated():
-             currentUser = UserProfile.objects.get(user=request.user)
-             upload_form = UserProfileForm(data = request.POST,)
-             if upload_form.is_valid():
-                 upload = upload_form.save(commit=False)
-                 if 'picture' in request.FILES:
+    if request.method == "POST":
+        if request.user.is_authenticated():
+            currentUser = UserProfile.objects.get(user=request.user)
+            upload_form = UserProfileForm(data=request.POST, )
+            if upload_form.is_valid():
+                upload = upload_form.save(commit=False)
+                if 'picture' in request.FILES:
                     upload.picture = request.FILES['picture']
 
                     currentUser.picture = upload.picture
                     currentUser.save()
                     return HttpResponseRedirect('/guess_the_movie/profile')
-                 else:
+                else:
                     print "Upload unsuccessful"
-                    return render(request, 'guess_the_movie/upload_picture.html', {"fail":True})
+                    return render(request, 'guess_the_movie/upload_picture.html', {"fail": True})
 
-             else:
-                 print upload_form.errors
-     else:
+            else:
+                print upload_form.errors
+    else:
         upload_form = UserProfileForm()
         return render(request, "guess_the_movie/upload_picture.html", {'upload_form': upload_form})
 
-def change_password(request):
-            change = False
-            if request.method == 'POST':
-                    resetForm = resetPasswordForm(data = request.POST,)
-                    if resetForm.is_valid():
-                        reset = resetForm.save()
-                        reset.set_password(reset.password)
-                        reset.save()
 
-                        change = True
-                    else:
-                        print resetForm.errors
-            else:
-                resetForm = resetPasswordForm()
-            return render(request,
-            'guess_the_movie/change_password.html',
-            {'resetForm': resetForm, 'change': change})
+def change_password(request):
+    change = False
+    if request.method == 'POST':
+        resetForm = resetPasswordForm(data=request.POST, )
+        if resetForm.is_valid():
+            reset = resetForm.save()
+            reset.set_password(reset.password)
+            reset.save()
+
+            change = True
+        else:
+            print resetForm.errors
+    else:
+        resetForm = resetPasswordForm()
+    return render(request,
+                  'guess_the_movie/change_password.html',
+                  {'resetForm': resetForm, 'change': change})
